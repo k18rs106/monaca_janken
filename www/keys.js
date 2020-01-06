8d6ac1099eb9a9bcf021b4ad0c80246a0d039446
k18rs106 <k18rs106@st.kyusan-u.ac.jp>
2020-01-06 14:15:06 +0900

    コイントス

--- www/keys.js
+++ www/keys.js
@@ -1,3 +1,4 @@
 // This is a JavaScript file
-var appKey = "";
-var clientKey = "";
+
+var appKey = "92f8a5ab84fc2857cec70d02d0f082c807754414498e66b4f30017aa3e69f0ec";
+var clientKey = "f19226f7fc53db59e013a1bb59b422122b52b2cbd85e6b8c5dfca64549cc9c15";
